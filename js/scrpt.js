9454e07fca5988863b57575a5e08df81e6f237c9
Oleg Troian <sleite4r@gmail.com>
2024-12-24 15:15:00 +0100

    modal-window for reservation-table

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,68 +1,16 @@
-// quantity-Input
+// MODAL-ABOUT
 
-var QtyInput = (function () {
-  var $qtyInputs = $(".qty-input");
+(() => {
+  const modal = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-  if (!$qtyInputs.length) {
-    return;
+  modal.openModalBtn.addEventListener("click", toggleModal);
+  modal.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    modal.modal.classList.toggle("is-hidden");
   }
-
-  var $inputs = $qtyInputs.find(".product-qty");
-  var $countBtn = $qtyInputs.find(".qty-count");
-  var qtyMin = parseInt($inputs.attr("min"));
-  var qtyMax = parseInt($inputs.attr("max"));
-
-  $inputs.change(function () {
-    var $this = $(this);
-    var $minusBtn = $this.siblings(".qty-count--minus");
-    var $addBtn = $this.siblings(".qty-count--add");
-    var qty = parseInt($this.val());
-
-    if (isNaN(qty) || qty <= qtyMin) {
-      $this.val(qtyMin);
-      $minusBtn.attr("disabled", true);
-    } else {
-      $minusBtn.attr("disabled", false);
-
-      if (qty >= qtyMax) {
-        $this.val(qtyMax);
-        $addBtn.attr("disabled", true);
-      } else {
-        $this.val(qty);
-        $addBtn.attr("disabled", false);
-      }
-    }
-  });
-
-  $countBtn.click(function () {
-    var operator = this.dataset.action;
-    var $this = $(this);
-    var $input = $this.siblings(".product-qty");
-    var qty = parseInt($input.val());
-
-    if (operator == "add") {
-      qty += 1;
-      if (qty >= qtyMin + 1) {
-        $this.siblings(".qty-count--minus").attr("disabled", false);
-      }
-
-      if (qty >= qtyMax) {
-        $this.attr("disabled", true);
-      }
-    } else {
-      qty = qty <= qtyMin ? qtyMin : (qty -= 1);
-
-      if (qty == qtyMin) {
-        $this.attr("disabled", true);
-      }
-
-      if (qty < qtyMax) {
-        $this.siblings(".qty-count--add").attr("disabled", false);
-      }
-    }
-
-    $input.val(qty);
-  });
 })();
-
-/*  */
